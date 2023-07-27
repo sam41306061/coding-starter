a5e491ddd5a19417d1da2da2de235286af30dbdf
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-07-27 17:40:32 -0600

    additions made to frontend, building out spec for contract

--- src/backend/contracts/AaveDeFiApp.sol
+++ src/backend/contracts/AaveDeFiApp.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.12;
 
 import {DataTypes} from './DataTypes.sol';
@@ -6,9 +7,35 @@ import {ILendingPoolAddressesProvider} from './ILendingPoolAddressesProvider.sol
 import {IPriceOracle} from './IPriceOracle.sol';
 import {IWETHGateway} from './IWETHGateway.sol';
 
+contract AaveDeFiApp  {
+    address public asset;
+    uint256 public amount; 
+    address public to; 
 
-contract AaveDeFiApp {
-    function HelloWorld() public pure returns (string memory) {
-        return "Hello World";
+
+
+    // get the weth address
+
+
+    // event for deposit
+    event Deposit (
+        address asset,
+        uint256 amount, 
+        address onBehalfOf,
+        uint16 referralCode
+        );
+    event WithDraw (
+        address reserve,
+        address user,
+        address to,
+        uint256 amount
+    );
+    // event for withdraw 
+    // event for trade
+
+
+
+    function deposit(address _asset, uint256 _amount, address _onBehalfOf, uint16 _referralCode) public {
+        emit Deposit(_asset, _amount, _onBehalfOf, _referralCode);
     }
 }
--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import logo from "./logo.png";
-import "./App.css";
 import Web3 from "web3";
 
 
@@ -34,19 +33,20 @@ const App = () => {
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           target="_blank"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://docs.aave.com/developers/getting-started/readme"
           onClick={loadWeb3}
           rel="noopener noreferrer"
         >
           AAVE Connect
         </a>
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0">Account:</a>
       </nav>
       <div className="container-fluid mt-5">
         <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
+          <main role="main" className="col-lg-12 text-center">
             <div className="content mr-auto ml-auto">
               <a
-                href="http://www.dappuniversity.com/bootcamp"
+                href="https://docs.aave.com/developers/getting-started/readme"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -69,47 +69,5 @@ const App = () => {
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div>
-//       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-//         <a
-//           className="navbar-brand col-sm-3 col-md-2 ms-3"
-//           href="http://www.dappuniversity.com/bootcamp"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           AAVE DAI
-//         </a>
-//       </nav>
-//       <div className="container-fluid mt-5">
-//         <div className="row">
-//           <main role="main" className="col-lg-12 d-flex text-center">
-//             <div className="content mx-auto mt-5">
-//               <a
-//                 href="http://www.dappuniversity.com/bootcamp"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <img src={logo} className="App-logo" alt="logo" />
-//               </a>
-//               <div className="content mx-auto mt-5">
-//                 {/* Added Ethereum Balance heading */}
-//                 <h2>Ethereum Balance:</h2>
-//                 <p>10 eth</p>
-//                 {/* Added DAI Balance heading */}
-//                 <h2>DAI Balance:</h2>
-//                 <p>1000 DAI</p>
-//                 {/* Added Borrow DAI button */}
-//                 <button onClick={() => alert("Borrow DAI")}>Borrow DAI</button>
-//               </div>
-//             </div>
-//           </main>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default App;
